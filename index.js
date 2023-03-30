96f504d9e0a4e34b1899f8b7a7d862310684f678
Ernest <ernestwinata@gmail.com>
2023-03-30 08:06:09 -0700

    creates myfile.md

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+function superbowlWin(year) {
+    const winYear = year.find(score => score.result === "W");
+    return winYear ? winYear.year : undefined;
+  }
+
+  
+  
+  
+  
